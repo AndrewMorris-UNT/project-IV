514fba42bd6305b4862630e40a840e76e15188a4
Andrew Morris <andrew.morris@unt.edu>
2014-12-09 19:34:34 -0600

    Changing routes and establishing entries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209031720) do
+ActiveRecord::Schema.define(version: 20141209055445) do
+
+  create_table "entries", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
